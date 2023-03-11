c9a46041109cab40bd75b7225da3f48e8511b692
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-11 16:43:34 +0800

    修改

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>欢迎访问此系统</h1>
     <el-button @click="logOut" size="large" class="btnStyle">退出登录</el-button>
   </div>
 </template>
@@ -17,3 +17,8 @@ const logOut = () => {
   router.push('/login')
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  text-align: center;
+}
+</style>
